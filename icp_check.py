0feb33ee3de51726a8ecbb6d18e567c5c7619425
Tr2ck <41378495+Tr2ck@users.noreply.github.com>
2022-02-12 09:37:28 +0800

    Create icp_check.py

---
+++ icp_check.py
@@ -0,0 +1,27 @@
+import requests
+from lxml import etree
+import re
+import time
+
+
+def icp(domain,line):
+	try:
+		url="http://icp.chinaz.com/"+domain
+		res = requests.get(url=url)
+		tree = etree.HTML(res.content.decode('utf-8'))
+		icp_name = tree.xpath('//a/text()')[368]
+		icp_type = tree.xpath('//strong/text()')[0]
+		print(icp_name+"    "+icp_type+"      "+line)
+		f = open('result.txt','a+')
+		f.write(icp_name+"    "+icp_type+"      "+line+"\n")
+		f.close()
+		time.sleep(2)
+	except:
+		pass
+	
+if __name__ == "__main__":
+	for line in open('domain.txt'):
+		domain=re.findall('[^/"><\.]{3,10}\.com|[^/"><\.]{4,}\.cn|[^/"><\.]{3,10}\.net\.cn\.top', line)
+		if domain:
+			icp(domain[0],line.replace('\n',''))
+        	
